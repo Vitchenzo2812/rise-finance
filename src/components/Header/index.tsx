d68e3fe36396bca8fad8c706a048a096866fa23d
Vitchenzo2812 <gvitchenzo@gmail.com>
2022-10-03 18:24:11 -0300

    feat: side navbar functional completed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Section from '../Section';
+
 import * as S from './styled';
 
 export default function Header() {
@@ -7,12 +7,40 @@ export default function Header() {
   
   return(
     <S.Wrapper>
-      <S.Logo />
-      <S.Menu isOpened={isOpened} onClick={() => setIsOpened(old => !old)}>
-        <div></div>
-        <div></div>
-        <div></div>
-      </S.Menu>
+      <S.Navbar>
+        <S.Logo href="/" />
+        <S.Menu isOpened={isOpened} onClick={() => setIsOpened(old => !old)}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </S.Menu>
+      
+        <S.NavList isOpened={isOpened}>
+          <S.NavItems>
+            <S.NavItem>
+              Consulting For Startups
+            </S.NavItem>
+          </S.NavItems>
+
+          <S.NavItems>
+            <S.NavItem>
+              Delivering Value
+            </S.NavItem>  
+          </S.NavItems>
+
+          <S.NavItems>
+            <S.NavItem>
+              Case Study
+            </S.NavItem>
+          </S.NavItems>
+
+          <S.NavItems>
+            <S.NavItem>
+              Coruma Finance
+            </S.NavItem>
+          </S.NavItems>
+        </S.NavList>
+      </S.Navbar>
     </S.Wrapper>
 );
 }
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,17 +1,16 @@
 import styled, { css } from "styled-components";
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.header`
   display: flex;
   position: sticky;
+
+  /* overflow: hidden; */
 
   top:0;
   left:0;
 
   width: 100%;
   height: max-content;
-
-  align-items: center;
-  justify-content: space-between;
 
   padding: 3rem 9rem;
 
@@ -24,13 +23,19 @@ export const Wrapper = styled.div`
   
 `
 
-export const Logo = styled.div`
+export const Logo = styled.a`
   width: 37.8rem;
   height: 4rem;
 
   background-image: url(./public/logo-rise-finance.svg);
   background-repeat: no-repeat;
   background-size: cover;
+
+  transition: 0.5s;
+
+  :hover {
+    opacity: 0.7;
+  }
 `
 
 export const Menu = styled.div<{ isOpened: boolean }>`
@@ -41,6 +46,13 @@ export const Menu = styled.div<{ isOpened: boolean }>`
   flex-direction: column;
   row-gap: 1rem;
   position: relative;
+
+  transition: 0.5s;
+
+  :hover {
+    opacity: 0.7;
+  }
+
   div {
     height: .3rem;
     width: 4.4rem;
@@ -59,4 +71,61 @@ export const Menu = styled.div<{ isOpened: boolean }>`
     transform: rotate(45deg) translateY(-.9rem);
   }
   ` : ''}
+`
+
+export const Navbar = styled.nav`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const NavList = styled.ul<{ isOpened: boolean }>`
+  width: 25vw;
+  height: 92vh;
+
+  border-top: 5px solid #020024;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+
+  position: absolute;
+  top: 13.2vh;
+  right: 0;
+
+  list-style: none;
+
+  background-image: url(./public/hero.svg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  opacity: 0.9;
+
+  transition: transform 0.3s ease-in-out;
+  
+  ${props => props.isOpened ? css`
+  transform: translateX(0%);
+  ` 
+  : css`
+  transform: translateX(100%);
+  `}
+`
+
+export const NavItems = styled.li`
+  letter-spacing: 3px;
+`
+
+export const NavItem = styled.a`
+  color: #fff;
+  text-decoration: none;
+  font-size: 1.5rem;
+
+  cursor: pointer;
+
+  transition: 0.5s;
+
+  :hover {
+    opacity: 0.3;
+  }
 `
